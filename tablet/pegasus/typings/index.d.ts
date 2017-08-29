915a2a9eec533eed8112354b599b95f7a0f3828d
sourabhgrover1 <sourabh.grover@prosense.co.in>
2017-08-29 16:01:13 +0530

    clean up

--- tablet/pegasus/typings/index.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="globals/moment/index.d.ts" />
-/// <reference path="modules/lodash/index.d.ts" />
--- tablet/pegasus/typings/main.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="main/ambient/es6-shim/index.d.ts" />
-/// <reference path="modules/lodash/index.d.ts" />
